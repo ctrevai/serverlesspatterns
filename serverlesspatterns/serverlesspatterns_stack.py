5cbdf0b209e36909bea76c1e1cf1d7c420a9d26a
ctrevai <ctrevai@gmail.com>
2024-08-26 14:32:25 +0000

    initial commit

--- serverlesspatterns/serverlesspatterns_stack.py
+++ serverlesspatterns/serverlesspatterns_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    RemovalPolicy,
+    aws_lambda as lambda_,
+    
 )
 from constructs import Construct
 
@@ -10,10 +12,36 @@ class ServerlesspatternsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ServerlesspatternsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        table = dynamodb.Table(
+            self,
+            "serverless_workshop_intro",
+            partition_key=dynamodb.Attribute(
+                name="_id",
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY
+            )
+        
+        lambda_function = lambda_.Function(
+            self,
+            "first_function",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="lambda_function.lambda_handler",
+            code=lambda_.Code.from_asset("lambda"),
+        )
+        
+        batch_function = lambda_.Function(
+            self,
+            "m1-add-sample-data",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="m1-add-sample-data.lambda_handler",
+            code=lambda_.Code.from_asset("lambda"),
+            # environment
+            environment={
+                "TABLE_NAME": table.table_name
+            }
+        )
+        
+        table.grant_full_access(batch_function)
+        
